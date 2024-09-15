4df827aba8713b430b34af71767c9eecc2b8ed20
KarlSupertramp <KarlUweMartin@gmail.com>
2024-09-15 11:08:54 +0200

    Cors test

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
 
     // Dynamic content loading
     const content = {
+        home: '<p>This is the content for Page 3.</p>',
         page1: '<iframe src="Pages/OrbitSandbox/index.html" allowfullscreen="true" width="100%" height="800px" style="border:none;"></iframe>',
         page2: '<p>This is the content for Page 2. You can load anything here, even another iframe.</p>',
         page3: '<p>This is the content for Page 3.</p>'
@@ -16,5 +17,16 @@ $(document).ready(function () {
         e.preventDefault();
         const page = $(this).data('page');
         $('#dynamic-content').html(content[page]);
+    });
+
+
+
+    // Dynamic content loading with CSS
+    $('.load-page').on('click', function (e) {
+        e.preventDefault();
+        const page = $(this).data('page');
+
+        // Load the corresponding HTML page into #dynamic-content
+        $('#dynamic-content').load('./pages/' + page + '/index.html');
     });
 });

5db85bfb5a96ecd9d7947649db92edc9435bcbd4
KarlSupertramp <KarlUweMartin@gmail.com>
2024-09-16 20:46:13 +0200

    testing cors shit

--- script.js
+++ script.js
@@ -4,29 +4,16 @@ $(document).ready(function () {
         $('#sidebar').toggleClass('active');
     });
 
-    // Dynamic content loading
-    const content = {
-        home: '<p>This is the content for Page 3.</p>',
-        page1: '<iframe src="Pages/OrbitSandbox/index.html" allowfullscreen="true" width="100%" height="800px" style="border:none;"></iframe>',
-        page2: '<p>This is the content for Page 2. You can load anything here, even another iframe.</p>',
-        page3: '<p>This is the content for Page 3.</p>'
-    };
-
     // When a link is clicked, load the respective content
     $('.load-content').on('click', function (e) {
         e.preventDefault();
-        const page = $(this).data('page');
-        $('#dynamic-content').html(content[page]);
-    });
+        const pageUrl = $(this).data('page'); // Assuming you pass a URL in the data-page attribute
 
-
-
-    // Dynamic content loading with CSS
-    $('.load-page').on('click', function (e) {
-        e.preventDefault();
-        const page = $(this).data('page');
-
-        // Load the corresponding HTML page into #dynamic-content
-        $('#dynamic-content').load('./pages/' + page + '/index.html');
+        // Load the HTML content dynamically via AJAX
+        $('#dynamic-content').load(pageUrl, function(response, status, xhr) {
+            if (status == "error") {
+                $('#dynamic-content').html("<p>Sorry, there was an error loading the page.</p>");
+            }
+        });
     });
 });
